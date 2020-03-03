7dd435dfcda42ce04e1705cf92ef37f84b07903a
little-huang <1124778999@qq.com>
2020-03-03 23:08:17 +0800

    init: electron-vue template

--- proxy/requestInterceptorConfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  sslConnectInterceptor: (req, cltSocket, head) => true,
-  requestInterceptor: (rOptions, req, res, ssl, next) => {
-      console.log(`正在访问：${rOptions.protocol}//${rOptions.hostname}:${rOptions.port}`);
-      console.log('cookie:', rOptions.headers.cookie);
-      res.end('hello node-mitmproxy!');
-      next();
-  }
-};
