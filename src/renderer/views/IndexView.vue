71e1eadfc414a10316c745d9c3a7b709089e1b39
little-huang <1124778999@qq.com>
2020-03-14 23:29:06 +0800

    fix: element table

--- src/renderer/views/IndexView.vue
+++ src/renderer/views/IndexView.vue
@@ -5,6 +5,30 @@
       <base-button @event="add">添加</base-button>
     </div>
     <!-- end header -->
+
+    <!-- start 列表 -->
+      <div>
+        <el-table
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="日期"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="姓名"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="地址">
+          </el-table-column>
+        </el-table>
+      </div>
+    <!-- end 列表 -->
+
   </div>
 </template>
 
@@ -13,7 +37,23 @@ export default {
   name: 'index-view',
   data() {
     return {
-
+      tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
     };
   },
   methods: {
